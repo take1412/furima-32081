9287f6bfd70c6d3da8674bad10912bcf768198cc
take1412 <taketake1412@yahoo.co.jp>
2020-11-27 17:57:20 +0900

    商品登録機能

---
+++ app/models/item.rb
@@ -0,0 +1,27 @@
+class Item < ApplicationRecord
+  has_one_attached :image
+  belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :charge
+  belongs_to :prefecture
+  belongs_to :days_ship
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :text
+    validates :price, inclusion: { in: 300..9999999, message: "is invalid. Please input between 300 and 9,999,999."}, format: { with: /\A[0-9]+\z/, message: "is invalid. Input nunber characters."}
+  end
+
+  with_options numericality: { other_than: 0, message: "can't be blank"} do
+    validates :category_id
+    validates :condition_id
+    validates :charge_id
+    validates :prefecture_id
+    validates :days_ship_id
+  end
+  
+end
