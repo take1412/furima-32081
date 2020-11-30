518a9dfd6636dfe32b53e64979361d596c15843e
take1412 <taketake1412@yahoo.co.jp>
2020-11-30 18:18:34 +0900

    商品情報編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:edit, :show]
+  before_action :move_to_index, only: :edit
+
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -18,7 +21,18 @@ class ItemsController < ApplicationController
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
     @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
   end
 
   private
@@ -26,4 +40,12 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :text, :category_id, :condition_id, :charge_id, :prefecture_id, :days_ship_id, :price).merge(user_id: current_user.id)
   end
+end
+
+def set_item
+  @item = Item.find(params[:id])
+end
+
+def move_to_index
+  redirect_to action: :index unless current_user.id == @item.user.id
 end
