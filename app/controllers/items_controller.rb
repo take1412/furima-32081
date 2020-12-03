b6d3e2cc98e083727b8386aceddec4628dc98567
take1412 <taketake1412@yahoo.co.jp>
2020-12-03 17:08:40 +0900

    商品情報編集機能のsoldout済みの商品の処理

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -54,6 +54,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to action: :index unless current_user.id == @item.user.id
+    redirect_to action: :index unless current_user.id == @item.user.id && @item.buy_info.blank?
   end
 end
